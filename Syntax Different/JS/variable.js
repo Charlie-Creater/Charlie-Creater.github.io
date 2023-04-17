41d550fab03cc55222767bd617ebbf626dcefd1e
Charlie-Creater <130728820+Charlie-Creater@users.noreply.github.com>
2023-04-17 13:12:24 +0800

    Add files via upload

---
+++ Syntax Different/JS/variable.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+//The var is the oldest keyword to declare a variable in JavaScript
+var z = [] 
+
+//The let keyword is an improved version of the var keyword
+let y = 1234567 
+
+//The const keyword has all the properties that are the same as the let keyword, except the user cannot update it
+const x = "Hello World" 
+
+=======
+//The var is the oldest keyword to declare a variable in JavaScript
+var z = [] 
+
+//The let keyword is an improved version of the var keyword
+let y = 1234567 
+
+//The const keyword has all the properties that are the same as the let keyword, except the user cannot update it
+const x = "Hello World" 
+
+>>>>>>> 77484d9 (introduction)
